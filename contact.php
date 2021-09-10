012e8929daa72c04b828255919967a87ad517ee0
Linberg07 <63363265+Linberg07@users.noreply.github.com>
2021-09-10 13:57:28 +0300

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,20 @@
+﻿<?php
+    $name = $_POST['name'];
+	$phone = $_POST['phone'];
+	$email = $_POST['email'];
+	
+	$to = "rocilead@gmail.com"; 
+	$date = date ("d.m.Y"); 
+	$time = date ("h:i");
+	$from = "StormGain";
+	$subject = "subject";
+
+	
+	$msg="
+    Имя: $name 
+    Телефон: $phone 
+    Email: $email"; 	
+	mail($to, $subject, $msg, "From: $from ");
+?>
+
+
---
+++ img/contact.php
@@ -0,0 +1,22 @@
+﻿<?php
+    $name = $_POST['name'];
+	$phone = $_POST['phone'];
+	$email = $_POST['email'];
+	$text = $_POST['comment'];
+	
+	$to = "alex.linberg07@gmail.com"; 
+	$date = date ("d.m.Y"); 
+	$time = date ("h:i");
+	$from = "Wecan.care";
+	$subject = "Wecan Partner";
+
+	
+	$msg="
+    Имя: $name 
+    Телефон: $phone 
+    Email: $email 
+    Комментарий: $text"; 	
+	mail($to, $subject, $msg, "From: $from ");
+?>
+
+
